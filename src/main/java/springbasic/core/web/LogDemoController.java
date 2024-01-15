a5570f20951858d489a7894fdcd829ed30bd87e2
hee9841 <lee98417716@gmail.com>
2024-01-15 17:40:16 +0900

    세션 9 - 스코프와 프록시

--- src/main/java/springbasic/core/web/LogDemoController.java
+++ src/main/java/springbasic/core/web/LogDemoController.java
@@ -14,15 +14,18 @@ public class LogDemoController {
 
     private final LogDemoService logDemoService;
     // myLogger를 주입받는게 아니라 myLogger를 찾을 수 있는 룩업을 주입 받게 됨
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         //동시에 여러 요청이 언다고 해도
         //요청마다 각각 객체를 따로 관리 해줌
-        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");

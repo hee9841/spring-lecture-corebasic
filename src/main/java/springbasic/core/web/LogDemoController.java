4b729298c288e56e151b7cca2e48ceed39e9472c
hee9841 <lee98417716@gmail.com>
2024-01-15 16:52:27 +0900

    세션 9 - 스코프와 Provider
    프로토타입 스코프 - 스코프와 Provider

--- src/main/java/springbasic/core/web/LogDemoController.java
+++ src/main/java/springbasic/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package springbasic.core.web;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,15 +13,21 @@ import springbasic.core.common.MyLogger;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    // myLogger를 주입받는게 아니라 myLogger를 찾을 수 있는 룩업을 주입 받게 됨
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request) {
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
+        //동시에 여러 요청이 언다고 해도
+        //요청마다 각각 객체를 따로 관리 해줌
+        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
+        Thread.sleep(1000);
+
         logDemoService.logic("testId");
 
         return "OK";
--- src/main/java/springbasic/core/web/LogDemoService.java
+++ src/main/java/springbasic/core/web/LogDemoService.java
@@ -1,6 +1,7 @@
 package springbasic.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import springbasic.core.common.MyLogger;
 
@@ -9,8 +10,9 @@ import springbasic.core.common.MyLogger;
 public class LogDemoService {
 
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
